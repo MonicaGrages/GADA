3fff6647a2537ce22b00a15ec12adeb2fce04a8c
MonicaGrages <mgrages1@gmail.com>
2017-08-07 20:20:22 -0400

    started event view and methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,9 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, :except => [:show, :index]
-  load_and_authorize_resource  only: [:create, :edit, :update, :destroy]
+  load_and_authorize_resource  only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @upcoming_events = Event.where('date >= ?', Date.today)
+  end
+
 end
