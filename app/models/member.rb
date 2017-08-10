ad3a05d1bc3980d16a337f1b1a1fcf0f9d578b85
MonicaGrages <mgrages1@gmail.com>
2017-08-10 14:09:50 -0400

    added member data validations front end and backend

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,8 @@
 class Member < ApplicationRecord
   validates :email, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :membership_type, presence: true
 
   before_save { email.downcase! }
 end
