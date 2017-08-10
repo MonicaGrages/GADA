b0d286fbb68eecce8bf35a68bb497c1e4fcf81ca
MonicaGrages <mgrages1@gmail.com>
2017-08-10 19:57:24 -0400

    added rails data validations to blog post and event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
 
   before_save { time.to_time }
 
