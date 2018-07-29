c747d534f03eb952d0c54e2cb5639362f5484551
Monica Grages <mgrages1@gmail.com>
2018-07-29 15:31:09 -0400

    Create member record for new successful transactions

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,22 +1,16 @@
 class WebhooksController < ApplicationController
   protect_from_forgery :except => [:create]
+
   def create
     response = validate_IPN_notification(request.raw_post)
-    puts "The response was #{response}."
     case response
     when "VERIFIED"
-      PaymentTransaction.create(payload: request)
-      # check that paymentStatus=Completed
-      # check that txnId has not been previously processed
-      # check that receiverEmail is your Primary PayPal email
-      # check that paymentAmount/paymentCurrency are correct
-      # process payment
+      PaymentTransaction.create(payload: params, status: 'verified')
+      create_membership(params) if verify_payment_details(params)
     when "INVALID"
-      PaymentTransaction.create(payload: request.raw_post)
-      PaymentTransaction.create(payload: params)
-      # log for investigation
+      PaymentTransaction.create(payload: params, status: 'invalid')
     else
-      # error
+      PaymentTransaction.create(payload: params, status: 'error')
     end
     head :ok
   end
@@ -34,5 +28,22 @@ class WebhooksController < ApplicationController
                          'Content-Length' => "#{raw.size}",
                          'User-Agent' => "Ruby-IPN-VerificationScript"
                        ).body
+  end
+
+  def verify_payment_details(params)
+    payment_completed = params['payment_status'] == 'Completed'
+    new_transaction = PaymentTransaction.where("payload ->> 'txn_id' = ?", params['txn_id']).blank?
+    receiver_matches = params['receiver_email'].downcase == 'treasurer@eatrightatlanta.org'
+    payment_completed && new_transaction && receiver_matches
+  end
+
+  def create_membership(params)
+    current_year = Time.now.year
+    exp_year = Time.now.month <= 5 ? current_year : current_year + 1
+    Member.create(first_name: params['first_name'],
+                  last_name: params['last_name'],
+                  email: params['payer_email'],
+                  membership_expiration_date: "#{exp_year}-05-31",
+                  membership_type: params['item_name1'].downcase.includes?('student') ? 'Student' : 'RD')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180728144353) do
+ActiveRecord::Schema.define(version: 20180729184425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20180728144353) do
     t.json "payload", default: {}
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "settings", force: :cascade do |t|
