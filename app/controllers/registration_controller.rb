d395c8a143ca363a200a443f24123bd9ac729627
Monica Grages <mgrages1@gmail.com>
2019-01-22 19:32:23 -0500

    Redirect to discounted membership when flip is on

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,14 +1,16 @@
 class RegistrationController < ApplicationController
   before_action :authenticate_user!, :except => [:discounted_membership, :success, :index]
 
+  def index
+    redirect_to discounted_membership_path if offering_discounted_membership?
+  end
+
   def discounted_membership
     month = Time.now.month
     year = Time.now.year
     @first_year = month <= 5 ? year : year + 1
     @second_year = month <= 5 ? year + 1 : year + 2
-    settings = Setting.first
-    @offering_discounted_membership = settings&.offer_discounted_membership
-    if @offering_discounted_membership
+    if offering_discounted_membership?
       render 'discounted_membership'
     else
       redirect_to registration_path
@@ -19,7 +21,12 @@ class RegistrationController < ApplicationController
   end
 
   def about_discounted_membership
+    offering_discounted_membership?
+  end
+
+  def offering_discounted_membership?
     settings = Setting.first
     @offering_discounted_membership = settings&.offer_discounted_membership
+    @offering_discounted_membership
   end
 end
