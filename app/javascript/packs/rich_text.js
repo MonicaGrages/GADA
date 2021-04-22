395eecd830fcb8abae4b133aef50c4df039f8dc7
Monica Finn <monicabfinn@gmail.com>
2021-04-22 14:34:03 -0700

    Monkeypatch Trix with superscript and subscript support

--- app/javascript/packs/rich_text.js
+++ app/javascript/packs/rich_text.js
@@ -1,2 +1,14 @@
-require("trix");
+import Trix from "trix";
 require("@rails/actiontext");
+
+Trix.config.textAttributes.sup = { tagName: "sup", inheritable: true };
+Trix.config.textAttributes.sub = { tagName: "sub", inheritable: true };
+
+
+addEventListener("trix-initialize", function(event) {
+  let buttonHTML  = '<button type="button" data-trix-attribute="sup" class="trix-button"><sup>SUP</sup></button>';
+  buttonHTML += '<button type="button" data-trix-attribute="sub" class="trix-button"><sub>SUB</sub></button>';
+
+  const buttonGroup = event.target.toolbarElement.querySelector(".trix-button-group--text-tools");
+  buttonGroup.insertAdjacentHTML("beforeend", buttonHTML)
+});
